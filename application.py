c224b109dbbdca339917989a16372c884515df4c
Naaz <naaz@macbookair.lan>
2025-03-16 16:06:28 -0400

    explit setting of port for deploymnet failure

--- application.py
+++ application.py
@@ -35,4 +35,5 @@ def index():
     return render_template("index.html" , prediction=None)
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0' , port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
